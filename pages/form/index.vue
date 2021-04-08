f315908407b1c2a3f959de5b833e83304b86b0d4
Hana Tokumoto <bubblesyo86@gmail.com>
2021-04-08 13:07:32 +0900

    add halfPoint.y

--- pages/form/index.vue
+++ pages/form/index.vue
@@ -3,6 +3,7 @@
     <h1>FORM</h1>
     <form action=""></form>
     <p>{{ width }} の半分は {{ halfWidth }}</p>
+    <P>half height {{ halfPoint.y }}</P>
   </div>
 </template>
 
@@ -10,13 +11,23 @@
 export default {
   data() {
     return {
-      width: 800
+      width: 800,
+      height: 500
     };
   },
   computed: {
-    // 算出プロパティhalfWidthを定義
     halfWidth: function() {
       return this.width / 2;
+    },
+    halfHeight: function() {
+      return this.height / 2;
+    },
+    // 「width × height」の中心座標をオブジェクトで返す
+    halfPoint: function() {
+      return {
+        x: this.halfWidth,
+        y: this.halfHeight
+      };
     }
   }
 };

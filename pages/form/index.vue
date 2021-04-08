a8f9fb373558f84b5a6446d20d01c803e72ef752
Hana Tokumoto <bubblesyo86@gmail.com>
2021-04-08 10:48:52 +0900

    add form

---
+++ pages/form/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="form-page">
+    <h1>FORM</h1>
+    <form action=""></form>
+    <p>{{ width }} の半分は {{ halfWidth }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      width: 800
+    };
+  },
+  computed: {
+    // 算出プロパティhalfWidthを定義
+    halfWidth: function() {
+      return this.width / 2;
+    }
+  }
+};
+</script>
+
+<style scoped></style>

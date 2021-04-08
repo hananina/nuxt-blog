7864b5cc07824c230e113ed09c1d9b3e0e2d43e1
Hana Tokumoto <bubblesyo86@gmail.com>
2021-04-08 15:08:45 +0900

    lodashとsortを追加

--- pages/form/index.vue
+++ pages/form/index.vue
@@ -2,60 +2,67 @@
   <div class="form-page">
     <h1>FORM</h1>
     <form action=""></form>
-
-    <div>width: {{ width }}</div>
-    <div>half width: {{ halfWidth }}</div>
-    <input type="text" v-model.numver="halfWidth" />
-
-    <p>算出プロパティ</p>
-    <ol>
-      <li>Original : <input type="text" v-model="computedNumber" /></li>
-      <li>Even/ Odd : {{ computedResult }}</li>
-    </ol>
-    <p>メソッド</p>
-    <ol>
-      <li>Original : <input type="text" v-model="methodNumber" /></li>
-      <li>Even/ Odd : {{ methodResult() }}</li>
-    </ol>
+    <ul>
+      <li v-for="item in list" :key="item.id">
+        {{ item.name }} {{ item.price }}円
+      </li>
+    </ul>
+    <input v-model.number="budget" /> 円以下に絞り込む
+    <input v-model.number="limit" /> 件を表示
+    <button :click="(sortedByAsc = !sortedByAsc)">
+      {{ sortedByAsc ? "降順↓へ切り替え" : "昇順↑に切り替え" }}
+    </button>
+    <p>
+      {{ matchedBudgetItems.length }} 件中、 {{ limitedItems.length }} 件を表示
+    </p>
+    <ul>
+      <li v-for="item in filteredList" :key="item.id">
+        {{ item.name }} {{ item.price }} 円
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
+
 export default {
   data() {
     return {
-      width: 800,
-      height: 500,
-      computedNumber: 4,
-      methodNumber: 4
+      sortedByAsc: false,
+      budget: 300,
+      limit: 2,
+      list: [
+        { id: 1, name: "りんご", price: 100 },
+        { id: 2, name: "バナナ", price: 145 },
+        { id: 3, name: "いちご", price: 380 },
+        { id: 4, name: "オレンジ", price: 200 },
+        { id: 5, name: "メロン", price: 900 }
+      ]
     };
   },
   computed: {
-    halfWidth: {
-      get: function() {
-        return this.width / 2;
-      },
-      set: function(val) {
-        this.width = val * 2;
-      }
+    matchedBudgetItems: function() {
+      return this.list.filter(item => {
+        item.price <= this.budget;
+      }, this);
     },
-    halfHeight: function() {
-      return this.height / 2;
+    sortedItems: function() {
+      return _.orderBy(
+        this.matchedBudgetItems,
+        "price",
+        this.sortedByAsc ? "asc" : "desc"
+      );
     },
-    computedResult: {
-      get() {
-        return this.computedNumber % 2 === 0 ? "Even" : "Odd";
-      },
-      set() {
-        return (this.computedNumber = this.computedNumber);
-      }
+    limitedItems: function() {
+      return this.sortedItems.slice(0, this.limit);
+    },
+
+    filteredList: function() {
+      return this.limitedItems;
     }
   },
-  methods: {
-    methodResult: function() {
-      return this.methodNumber % 2 === 0 ? "Even" : "Odd";
-    }
-  }
+  methods: {}
 };
 </script>
 

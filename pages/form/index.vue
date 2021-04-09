f97ee0fd640e4964240c50aebbbb42e55ae2f6d4
Hana Tokumoto <bubblesyo86@gmail.com>
2021-04-09 16:20:22 +0900

    add: focus a particular element with v-focus directive

--- pages/form/index.vue
+++ pages/form/index.vue
@@ -2,46 +2,20 @@
   <div class="form-page">
     <h1>FORM</h1>
     <form action=""></form>
-    <select v-model="current" name="" id="">
-      <option v-for="topic in topics" :value="topic.value" :key="topic.name">
-        {{ topic.name }}
-      </option>
-    </select>
-    <div>{{ current }}</div>
-    <ul>
-      <li v-for="item in list" :key="item.id">{{ item.id }}</li>
-    </ul>
+    <input type="text" v-focus />
   </div>
 </template>
 
 <script>
 import _ from "lodash";
-import axios from "axios";
 
 export default {
-  data() {
-    return {
-      list: [],
-      current: "",
-      topics: [
-        { value: "vue", name: "Vue.js" },
-        { value: "jQuery", name: "jQuery" }
-      ]
-    };
-  },
-  computed: {},
-  watch: {
-    current: function(val) {
-      // GitHubのAPIからトピックのリポジトリを検索
-      axios
-        .get("https://api.github.com/search/repositories", {
-          params: {
-            q: "topic:" + val
-          }
-        })
-        .then(response => {
-          this.list = response.data.items;
-        });
+  directives: {
+    focus: {
+      //紐付いている要素がDOMに挿入されるとき
+      inserted: el => {
+        el.focus(); //要素にfocusを当てる
+      }
     }
   }
 };

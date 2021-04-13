b0e3f446316784b3a7bad58ac7469eb2bd11d8ad
Hana Tokumoto <bubblesyo86@gmail.com>
2021-04-13 16:07:50 +0900

    add: open weather api

--- pages/weather/index.vue
+++ pages/weather/index.vue
@@ -1,33 +1,35 @@
 <template>
   <div>
-    <!-- <p>time:{{ data.time.updated }}</p>
-    <p>chartName:{{ data.chartName }}</p>
-    <p>USD:{{ data.bpi.USD.rate }}</p> -->
-    <p>weather:{{ data }}</p>
+    <ul>
+      <li>場所の名前: {{ data.name }}</li>
+      <li>Lon:{{ data.coord.lon }} Lat:{{ data.coord.lat }}</li>
+      <li>天気:{{ data.weather[0].description }}</li>
+      <li>温度:{{ data.main.temp }}</li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   // async asyncData({ $axios }) {
-  //   const ip = await $axios.$get('http://icanhazip.com')
-  //   return { ip }
+  //   const response = await $axios.$get(
+  //     "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=5f2899a04c5660712577c79c9ef9205b"
+  //   );
+  //   return { data: response };
   // }
-
   async asyncData({ $axios }) {
     const response = await $axios.$get(
-      "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=be56ce65affba360fcfb08d130f572b1"
+      "https://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          q: "boston",
+          appid: "5f2899a04c5660712577c79c9ef9205b",
+          lang: "ja"
+        }
+      }
     );
     return { data: response };
   }
-
-  // async asyncData({ $axios }) {
-  //   const response = await $axios.$get(
-  //     "https://api.coindesk.com/v1/bpi/currentprice.json"
-  //   );
-  //   return {
-  //     data: response
-  //   };
-  // }
 };
 </script>
